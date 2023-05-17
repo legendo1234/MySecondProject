b4a53160c27ed032748ddba2e94a68cf2c9fc8f4
legendo1234 <matej.weber@gmail.com>
2023-05-17 14:38:27 +0200

    Add project files.

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,27 @@
+﻿// Welcome user to App
+using System.ComponentModel;
+
+Console.WriteLine("Welcome to the greeting application");
+Console.WriteLine("This application was built by Matej");
+Console.WriteLine("-------------------------------------");
+Console.WriteLine();
+
+// Ask for first name
+Console.Write("What is your name: ");
+string firstName = Console.ReadLine() ;
+Console.WriteLine();
+Console.Write("What is your last name: ");
+string lastName = Console.ReadLine();
+Console.Write("How old are you: ");
+string age = Console.ReadLine();
+
+// Greet user by name
+Console.WriteLine();
+Console.WriteLine("Hello " + firstName + " " + lastName);
+Console.WriteLine();
+Console.WriteLine("You are " + age);
+Console.WriteLine("-------------------------------------");
+
+
+Console.WriteLine("Thank you for using my application Mr." + lastName + ".");
+Console.ReadLine();
